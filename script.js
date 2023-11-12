0f88ffdb4dcf5a346383cd2d6d1f56c3ae92bffb
szoky101 <markszokolai@gmail.com>
2023-11-12 13:23:06 +0100

    user data and the game is done

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-function getRandomInt(max){
-    return Math.floor(Math.random() * max)
-}
-
-function getComputerChoise(){
-    let random_szam = getRandomInt(3);
-    if (random_szam === 0)
-    {
-        return "rock";
+function game(){
+    function getRandomInt(max){
+        return Math.floor(Math.random() * max)
     }
-    else if(random_szam === 1)
-    {
-        return "scissors";
+
+    function getComputerChoise(){
+     let random_szam = getRandomInt(3);
+        if (random_szam === 0)
+        {
+            return "rock";
+        }
+        else if(random_szam === 1)
+        {   
+            return "scissors";
+        }
+        else
+        {
+            return "paper";
+        }
     }
-    else
-    {
-        return "paper";
+
+    function oneRoundGame(playerSelection, computerSelection){
+        if (computerSelection == playerSelection)
+        {
+            return "The game is draw"
+        }
+        else if ((computerSelection == "scissors" && playerSelection == "rock") ||
+                (computerSelection == "rock" && playerSelection == "paper") ||
+                (computerSelection == "paper" && playerSelection == "scissors")
+                                                                )
+        {   
+            return "you win";
+        }
+        else if ((computerSelection == "rock" && playerSelection == "scissors") ||
+                (computerSelection == "paper" && playerSelection == "rock") ||
+                (computerSelection == "scissors" && playerSelection == "paper")
+                                                                  )
+        {
+            return "You lose";
+        }
     }
-}
-
-function getPlayerChoise(){
-
-}
-
-function oneRoundGame(playerSelection, computerSelection){
+    let playerChoise = prompt("Choose a sign")
+    return oneRoundGame(playerChoise, getComputerChoise());
 
 }
 
-console.log(getComputerChoise())
+function gameloop(){
+    let i = 0;
+    while (i < 5)
+    {
+    let eredmeny = game();
+    console.log(eredmeny)
+    i++;
+    }
+
+}
+
+gameloop();
